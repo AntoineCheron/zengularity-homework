af039c7621bf4748e196f117b7193a1eb16b8321
AntoineCheron <cheron.antoine@gmail.com>
2017-08-26 19:41:28 +0200

    Implement UI for Register and Forgot Password components + routes

---
+++ src/components/ForgotPassword.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="forgot" class="login hero is-fullheight">
+        <div class="card is-paddingless login-card">
+            <div class="columns is-marginless">
+                <div class="column is-one-thirds login-form">
+                    <div>
+                        <h1>Zenelectricity</h1>
+                    </div>
+                    <div>
+                        <form>
+                            <div class="field">
+                                <p class="control has-icons-left">
+                                    <input v-model="email" class="input" type="email" placeholder="Email address">
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                                </p>
+                            </div>
+                            <div class="field">
+                                <p class="control">
+                                    <button @click="forgot" class="button green-button login-button">
+                                        recover password
+                                    </button>
+                                    <p v-if="error != ''" class="error-label">{{ error }}</p>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <a class="signup-link" @click="register">Create a new account !</a>
+                    </div>
+                </div>
+                <div class="column is-two-thirds login-thumbnail" id="forgot-thumbnail">
+                    <p id="forgot-title">{{ title }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return {
+      title: 'We will help you get your password back !',
+      email: '',
+      error: '',
+    };
+  },
+  methods: {
+    forgot() {
+      // TODO
+      alert('forgot');
+    },
+    register() {
+      this.$router.push('/register');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+    @import 'src/assets/css/user-login-commons';
+
+    #forgot {
+        #forgot-thumbnail {
+            background-image: url('../assets/img/forgot-bg.jpg');
+
+            &> p {
+                width: 410px;
+                top: calc(100% - 155px);
+            }
+        }
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Login from '@/components/Login';
+import ForgotPassword from '@/components/ForgotPassword';
+import Register from '@/components/Register';
 
 Vue.use(Router);
 
@@ -8,8 +10,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'App',
+      component: Login,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/forgot',
+      name: 'Forgot Password',
+      component: ForgotPassword,
+    },
+    {
+      path: '/register',
+      name: 'Create a new account',
+      component: Register,
     },
   ],
 });
