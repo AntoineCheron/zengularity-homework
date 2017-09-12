cf725156e40b4e7290d0d1a435adbf6470d8b886
AntoineCheron <cheron.antoine@gmail.com>
2017-09-12 19:14:07 +0200

    Modify mongoconfig to use vars from application.properties

--- src/main/java/fr/antoinecheron/zenelectricity/config/MongoConfig.java
+++ src/main/java/fr/antoinecheron/zenelectricity/config/MongoConfig.java
@@ -1,8 +1,10 @@
 package fr.antoinecheron.zenelectricity.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import fr.antoinecheron.zenelectricity.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -16,14 +18,22 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @EnableReactiveMongoRepositories(basePackageClasses = ApplicationUserRepository.class)
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String databaseUri;
+
     @Bean
     public MongoClient mongoClient () {
-        return MongoClients.create();
+        return MongoClients.create(new ConnectionString(databaseUri));
     }
 
     @Override
     protected String getDatabaseName () {
         return "zenelectricity-dev";
+    }
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return mongoClient();
     }
 
     @Bean
