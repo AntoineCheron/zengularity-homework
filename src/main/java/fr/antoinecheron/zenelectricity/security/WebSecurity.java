7dc68b90c3b5eae0f65d377bcb5aa22875ba9cc6
AntoineCheron <cheron.antoine@gmail.com>
2017-09-13 09:46:54 +0200

    Free access to swagger needed resources

--- src/main/java/fr/antoinecheron/zenelectricity/security/WebSecurity.java
+++ src/main/java/fr/antoinecheron/zenelectricity/security/WebSecurity.java
@@ -31,7 +31,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure (HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers( "/", "/static/**", "/swagger-ui.html", "/v2/api-docs/**", "/swagger-resources/**").permitAll()
+                .antMatchers( "/",
+                        "/static/**",
+                        "/swagger-ui.html",
+                        "/v2/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .logout().permitAll()
